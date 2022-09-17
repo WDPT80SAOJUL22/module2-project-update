74064d9cf6af7a8cc9748e102ce2a4f8f17f0417
Gabriel Sicuto <gabrielsicuto@gmail.com>
2022-09-17 12:10:19 -0300

    feat: configure routes on App Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import EditProject from './pages/EditProject';
+import Home from './pages/Home';
+import NewProject from './pages/NewProject';
+import ProjectDetail from './pages/ProjectDetail';
 
  const App = () => {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/new-project' element={<NewProject/>}/>
+        <Route path='/project/:id' element={<ProjectDetail/>}/>
+        <Route path='/edit-project/:id' element={<EditProject/>}/>
+      </Routes>
     </div>
   );
 }
