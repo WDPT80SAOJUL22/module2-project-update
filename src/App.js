a06960d44e5976dad9df5d68fae74073667ff672
Gabriel Sicuto <gabrielsicuto@gmail.com>
2022-09-20 20:09:23 -0300

    feat: Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import {EditProject, Home, NewProject, ProjectDetail} from './pages'
+import { EditProject, Home, NewProject, ProjectDetail } from './pages';
+import { NavBar } from './components/NavBar';
 
- const App = () => {
+const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/new-project' element={<NewProject/>}/>
-        <Route path='/project/:id' element={<ProjectDetail/>}/>
-        <Route path='/edit-project/:id' element={<EditProject/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/new-project' element={<NewProject />} />
+        <Route path='/project/:id' element={<ProjectDetail />} />
+        <Route path='/edit-project/:id' element={<EditProject />} />
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
